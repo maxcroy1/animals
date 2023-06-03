adb0f34ee6b18d8e34893dc8ff83083de995be49
Maxwell Croy <max.croy1@gmail.com>
2023-06-03 16:54:08 -0500

    Added images

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,7 +1,25 @@
+import bird from "./svg/bird.svg";
+import cat from "./svg/cat.svg";
+import cow from "./svg/cow.svg";
+import dog from "./svg/dog.svg";
+import gator from "./svg/gator.svg";
+import heart from "./svg/heart.svg";
+import horse from "./svg/horse.svg";
+
+const animalMap = {
+  bird,
+  cat,
+  cow,
+  dog,
+  gator,
+  heart,
+  horse
+};
+
 const AnimalShow = ({ type }) => {
   return (
     <div>
-      <p>{type}</p>
+      <img src={ animalMap[type] } alt={ type } />
     </div>
   );
 }
